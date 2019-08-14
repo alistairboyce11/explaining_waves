cd3129adba0c513b49e15ba1cc8e6d5588694a9f
Matthew Harding (SC) Part III Jan 2019 <mh826@syrma.esc.cam.ac.uk>
2019-08-14 13:38:24 +0000

    Reverting changes

--- body_wave_predictions_wavefronts.py
+++ body_wave_predictions_wavefronts.py
@@ -17,22 +17,9 @@ from obspy.taup import plot_ray_paths
 
 import matplotlib
 from matplotlib.animation import FuncAnimation
-#from matplotlib.animation import PillowWriter
+
 # More about the obspy routines we are using can be found here:
 # https://docs.obspy.org/packages/obspy.taup.html
-
-# more imports
-import instaseis
-from obspy import read
-import sys,glob
-import obspy.signal.rotate
-from obspy import UTCDateTime
-import obspy.geodetics.base
-import numpy as np
-import obspy.geodetics
-from IPython.display import HTML, Image
-matplotlib.rc('animation', html='html5')
-import matplotlib.patches as patches
 
 # velocity model as a function of depth.
 model = TauPyModel(model='ak135')
@@ -54,11 +41,6 @@ rays_dist_max=[110, 150,  181, 181, 160,180, 180 ]
 # this should be adapted, colors should change at reflection points
 color_attenuation=[1.0, 0.5, 0.8, 0.8, 0.3, 0.8, 1.0]
 
-# Receiver station latitude and longitude
-
-stlatitude=12
-stlongitude=56.78
-
 # Direct P-wave test
 # phases_to_plot =['P']
 # rays_dist_min=[0]
@@ -75,7 +57,8 @@ depth_earthquake= 30.
 radius = 6371.
 
 # regular time array in s, 1 s resolution
-time = np.arange(0., 3600.)
+time = np.arange(0., 2000.)
+
 
 # calculate through and save ray paths in interpolated time domain
 save_paths=[]
@@ -107,7 +90,7 @@ lines_right=[]
 # start plot for t= 0
 fig = plt.figure(figsize =(10,5))
 # define polar subplot
-ax = plt.subplot(1,2,1, polar = True)
+ax = plt.subplot(1,1,1, polar = True)
 ax.set_theta_zero_location('N')
 ax.set_theta_direction(-1)
 ax.set_xticks([])
@@ -146,97 +129,6 @@ ax.set_rmax(radius)
 ax.set_rmin(0.0)
 # plt.show()
 
-# Generating seismogram
-def generate_seismogram(evtlatitude=3.0900, evtlongitude=94.2600, evtdepth=28.610, stlatitude=stlatitude,
-stlongitude=stlongitude):
-
-        clean = True
-        # Load database with Green Functions
-        # db = instaseis.open_db("syngine://prem_a_2s")
-        # db = instaseis.open_db("syngine://ak135f_1s")
-        db = instaseis.open_db("syngine://ak135f_2s ")
-
-    # Read in source
-        source = instaseis.Source(latitude=evtlatitude, longitude=evtlongitude, depth_in_m=evtdepth,
-                          m_rr = 1.040000e+29,
-                          m_tt = -4.270000e+28,
-                          m_pp = -6.100000e+28,
-                          m_rt = 2.980000e+29,
-                          m_rp = -2.400000e+29,
-                          m_tp = 4.260000e+28,
-                          origin_time=UTCDateTime('2004-12-26 00:58:50'))
-
-# Other option for source definition
-# source = instaseis.Source.from_strike_dip_rake(latitude=10.0, longitude=12.0, depth_in_m=1000, strike=79,dip=10, rake=20, M0=1E17)
-
-# Station parameters
-        receiver = instaseis.Receiver(latitude=stlatitude, longitude=stlongitude, network="AB",station="CDE", location="SY")
-
-# compute raypath statistics
-        distm, az, baz = obspy.geodetics.base.gps2dist_azimuth(evtlatitude, evtlongitude, stlatitude, stlongitude)
-        distdg = distm / (6371.e3 * np.pi / 180.)
-
-        start =UTCDateTime('2004-12-26 00:58:50')
-        end   =UTCDateTime('2004-12-26 00:58:50')+3600  
-
-# Run function to get waveforms
-        st = db.get_seismograms(source=source, receiver=receiver,kind='displacement', dt=0.1)
-# Add headers to Pickle in .stats
-        st[0].stats['evla'] = evtlatitude
-        st[0].stats['evlo'] = evtlongitude
-        st[0].stats['evdp'] = evtdepth
-        st[0].stats['stla'] = stlatitude
-        st[0].stats['stlo'] = stlongitude
-        st[0].stats['dist'] = distdg
-        st[0].stats['az'] = az
-        st[0].stats['baz'] = baz
-
-# Rotate synthetics to radial and transverse
-        stE = st.select(channel='BXE')
-        stN = st.select(channel='BXN')
-        stZ = st.select(channel='BXZ')
-        [stRtmp,stTtmp]=obspy.signal.rotate.rotate_ne_rt(stN[0].data,stE[0].data,st[0].stats['baz'])
-        stR=stN[0].copy()
-        stR.stats['channel']='BXR'
-        stR.data = stRtmp
-        stT=stN[0].copy()
-        stT.stats['channel']='BXT'
-        stT.data = stTtmp
-
-        st+=stR
-        st+=stT
-
-#OVERWRITES previous PICKLE with synthetics included
-        st.write('../waveform_movie_outputs/seis_synth.PICKLE',format='PICKLE')
-   
-        return
-
-# Generating seismogram subplot
-st1=read('../waveform_movie_outputs/seis_synth.PICKLE',format='PICKLE')
-ax1 = plt.subplot(1, 2, 2)
-st1Z=st1.select(channel='BXZ')
-seis_times = st1Z[0].times("matplotlib")
-data = st1Z[0].data
-seis, = ax1.plot(seis_times, data, "r-")
-ax1.xaxis_date()
-fig.autofmt_xdate()
-
-#Add rectangle as reference frame
-direct_P_travel_time = model.get_travel_times(source_depth_in_km=depth_earthquake, distance_in_degree=stlatitude, phase_list=["P"])[0].time
-bottom, top = plt.ylim()
-height  = top - bottom
-start, end = plt.xlim()
-duration = end - start
-width = duration/10
-start_time=(direct_P_travel_time/3600)*duration
-rect = patches.Rectangle((end-start_time-width,bottom), width, height, linewidth=1,facecolor='None', edgecolor='black', zorder=10)
-start_index = np.where(seis_times>=rect.xy[0])[0][0]
-end_index = np.where(seis_times>=(rect.xy[0]+width))[0][0]
-ax1.add_patch(rect)
-
-frame_number = 250
-frame_rate = 25
-
 def animate(t, lines_left, lines_right):
     '''
         Function updates lines with time
@@ -259,24 +151,7 @@ def animate(t, lines_left, lines_right):
         cols = [col, col,col]
         line.set_color(cols)
         lines_left[l].set_color(cols)
-
-    new_data = np.zeros(data.shape)
-    speed = round(len(data)/3600) # pieces of data per frame
-    if t < len(data)/speed:
-        new_data = np.array(data[:round(speed*(t))])
-        new_data = np.pad(new_data, (len(data)-len(new_data),0), mode='constant')
-        new_data = new_data[start_index:end_index]
-        new_data = np.pad(new_data, (start_index, len(data)-end_index), mode='constant')
-    elif t < (2*len(data))/speed:
-        new_data = np.array(data[round(speed*(t)) - len(data):])
-        new_data = np.pad(new_data, (0,len(data)-len(new_data)), mode='constant')
-        new_data = new_data[start_index:end_index]
-        new_data = np.pad(new_data, (start_index, len(data)-end_index), mode='constant')
-    else:
-        new_data = np.zeros(data.shape)
-    seis.set_data(seis_times, new_data)
-    ax1.add_patch(rect)
-    return(line,seis)
+    return(line,)
 
 # Sets up animation
 animation = FuncAnimation(
@@ -285,14 +160,15 @@ animation = FuncAnimation(
                           # The function that does the updating of the Figure
                           animate,
                           # Frame information (here just frame number)
-                          frame_number,
+                          np.arange(1000),
                           # Extra arguments to the animate function
                           fargs=[lines_left, lines_right],
                           # Frame-time in ms; i.e. for a given frame-rate x, 1000/x
-                          interval=1000/frame_rate
+                          interval=1000 / 25
                           )
 
 # to save as GIF :
-#animation.save("../wavefrom_movie_outputs/out_funcanimation.gif", writer=PillowWriter(fps=24))
-animation.save("../waveform_movie_outputs/seis_movie.gif", writer='imagemagick')
+from matplotlib.animation import PillowWriter
+animation.save("../wavefrom_movie_outputs/out_funcanimation.gif", writer=PillowWriter(fps=24))
+
 
